fd55b76da7429d9bac72a019420a6e1e561b5069
Troy Domio <troyakins99@yahoo.com>
2020-05-06 02:16:33 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,8 +1,8 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
-def meal_choice (veg1, veg2, protein="tofu")
-print "A plate of #{protein} with #{veg1} and #{veg2}."
+def troy (name)
+ my #{name} is Troy. 
 end
 
 
