5df15def03f1512d19fa453ab8960b880e4ed307
Troy Domio <troyakins99@yahoo.com>
2020-05-06 02:14:56 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,6 +5,7 @@ def meal_choice (veg1, veg2, protein="tofu")
  puts "What a nutritious meal!"
 puts "A plate of #{protein} with #{veg1} and #{veg2}."
 return meal_choice (veg1, veg2, protein="tofu")
+puts meal_choice (veg1, veg2, protein="tofu")
 end
 
 
